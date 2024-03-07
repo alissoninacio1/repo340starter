6ae5f9f2d777121913aa44261a3c5987dbd7f8b6
alissoninacio1 <alissoninacio96@gmail.com>
2024-03-07 15:44:15 -0700

    cse340 changed

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -2,7 +2,7 @@ CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
 
 ALTER TYPE public.account_type
-    OWNER TO cse340aoi;
+    OWNER TO cse340;
 
 
 -- create relationship between 'classification' and 'inventory' tables
