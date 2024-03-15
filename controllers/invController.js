9f378ee67a3491c8bb7c5717f85de86b23d94dbe
alissoninacio1 <alissoninacio96@gmail.com>
2024-03-15 12:04:45 -0700

    inv controlller updated

--- controllers/invController.js
+++ controllers/invController.js
@@ -19,4 +19,34 @@ invCont.buildByClassificationId = async function (req, res, next) {
   })
 }
 
+
+/* ***************************
+ *  Build a specific inventory item detail view
+ * ************************** */
+invCont.buildViewVehicleDetail = async (req, res) => {
+  try {
+    const vehicle_id = req.params.id;
+    const data = await invModel.retrieveVehicleDataById(vehicle_id);
+
+    const grid = await Util.buildVehicleDetailview(data);
+
+    let nav = await Util.getNavigation();
+
+    if (data.length > 0) {
+      const vehicleTitle = `${data[0].inv_make} ${data[0].inv_model}`;
+      res.render('inventory/detail', { 
+        title: vehicleTitle, 
+        nav, 
+        grid, 
+      });
+    } else {
+      res.status(404).send('Vehicle not found');
+    }
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
 module.exports = invCont
