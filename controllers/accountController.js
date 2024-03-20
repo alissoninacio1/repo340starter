63cfe88c9966fd01659b027dcb2226de751832a3
alissoninacio1 <alissoninacio96@gmail.com>
2024-03-20 14:28:30 -0700

    files added

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -19,18 +19,28 @@ async function buildLogin(req, res, next) {
 /* ****************************************
 *  Deliver registration view
 * *************************************** */
+// async function buildRegister(req, res, next) {
+//   try {
+//     let nav = await utilities.getNav();
+//     res.render("account/register", {
+//       title: "Register",
+//       nav,
+//     });
+//   } catch (error) {
+//     next(error);
+//   }
+// }
+/* ****************************************
+*  Deliver registration view
+* *************************************** */
 async function buildRegister(req, res, next) {
-  try {
-    let nav = await utilities.getNav();
-    res.render("account/register", {
-      title: "Register",
-      nav,
-    });
-  } catch (error) {
-    next(error);
-  }
+  let nav = await utilities.getNav()
+  res.render("account/register", {
+    title: "Register",
+    nav,
+    errors: null,
+  })
 }
-
 
 
 /* ****************************************
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const utilities = require("../utilities");
+const regValidate = require('../utilities/account-validation')
 const accountController = require("../controllers/accountController");
 
 // Route to deliver login view
@@ -11,5 +12,13 @@ router.get("/signup", utilities.handleErrors(accountController.buildRegister));
 
 //Route to send the fomr and register in the DB
 router.post('/register', utilities.handleErrors(accountController.registerAccount))
+
+// Process the registration data
+router.post(
+    "/register",
+    regValidate.registationRules(),
+    regValidate.checkRegData,
+    utilities.handleErrors(accountController.registerAccount)
+  )
 
 module.exports = router;
